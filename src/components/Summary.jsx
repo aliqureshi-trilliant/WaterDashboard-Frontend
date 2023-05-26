1c8428d889288dfc7b26676a4351f4c5e5bc5ed9
aliqureshi-trilliant <ali.qureshi@trilliant.com>
2023-05-26 13:38:21 -0400

    adding summary component

---
+++ src/components/Summary.jsx
@@ -0,0 +1,18 @@
+import classes from './Summary.module.css';
+import summaryImage from '/images/circuit.png';
+
+function Summary () {
+    return (
+        <div className={classes.summary}>
+            <div className={classes.summaryTextContainer}>
+                <h2 className={classes.summaryHeading}>Water MIU</h2>
+                <p className={classes.summaryText}>A small water meter with the capabilites of a water meter.</p>
+            </div>
+            <div className={classes.imageContainer}>
+                <img className={classes.summaryImage} src={summaryImage} alt="summaryImage" />
+            </div>
+        </div>
+    );
+}
+
+export default Summary;
