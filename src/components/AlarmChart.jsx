7b19d2d848ec02aca655ce1eae720a47068e28f6
aliqureshi-trilliant <ali.qureshi@trilliant.com>
2023-07-05 15:32:56 -0400

    Adding alarm data for a day to device page

--- src/components/AlarmChart.jsx
+++ src/components/AlarmChart.jsx
@@ -2,34 +2,13 @@ import classes from './AlarmChart.module.css';
 import { BsFillBellFill } from 'react-icons/bs';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
+import { useState, useEffect, useRef } from 'react';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import { BiError } from 'react-icons/bi';
+import { useParams } from 'react-router-dom';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
-
-const data = {
-    labels: ['High Flow','Back Flow','Failed Reads','Battery','Temperature'],
-    datasets: [
-        {
-            label: '',
-            data: [12, 19, 13, 6, 4],
-            backgroundColor: [
-                'rgb(120, 178, 119)',
-                'rgb(150, 222, 149)',
-                'rgb(42, 101, 42)',
-                'rgb(214, 237, 212)',
-                'rgb(40, 45, 41)',
-            ],
-            borderColor: [
-                'rgb(120, 178, 119)',
-                'rgb(150, 222, 149)',
-                'rgb(42, 101, 42)',
-                'rgb(214, 237, 212)',
-                'rgb(40, 45, 41)',
-            ],
-            borderWidth: 1,
-            cutout: '80%'
-        },
-    ],
-};
 
 const plugins = [{
     beforeDraw: function(chart) {
@@ -77,11 +56,103 @@ const options = {
 };
 
 function AlarmChart(props){
+    
+    const { deviceName } = useParams();
+    const [alarmData, setAlarmData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const chartReference = useRef(null);
 
     const additionalStyles = props.additionalStyles? props.additionalStyles : '';
 
-    return (
-        <div className={`${classes.card} ${additionalStyles}`}>
+    const fetchData = () => {
+        fetch(`http://localhost:3000/api/waterMIUs/${deviceName}/KPIs/alarms?days=1`)
+            .then((response) => {
+                if(!response.ok) {
+                    throw new Error(`Error ${response.status}: ${response.message}`);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setAlarmData(formatData(data));
+                setError(null);
+            })
+            .catch((error) => {
+                setError(error);
+                setAlarmData(null);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    };
+
+    const formatData = (data) => {
+        const dataArray = [0,0,0,0,0];
+        data.forEach((el) => {
+            switch(el.event_id) {
+            case '24.21.87.47': dataArray[2]++;break;
+            case '24.5.48.93': dataArray[0]++;break;
+            case '24.5.48.219': dataArray[1]++;break;
+            case '24.35.0.40': dataArray[4]++;break;
+            case '24.2.22.150': dataArray[3]++;break;
+            }
+        })
+        return dataArray;
+    };
+
+    const updateChart = (formattedData) => {
+        const chart = chartReference.current;
+        chart.data.datasets[0].data = formattedData;
+        chart.update();
+    }
+
+    const data = {
+        labels: ['High Flow','Back Flow','Failed Reads','Battery','Temperature'],
+        datasets: [
+            {
+                label: '',
+                data: alarmData,
+                backgroundColor: [
+                    'rgb(120, 178, 119)',
+                    'rgb(150, 222, 149)',
+                    'rgb(42, 101, 42)',
+                    'rgb(214, 237, 212)',
+                    'rgb(40, 45, 41)',
+                ],
+                borderColor: [
+                    'rgb(120, 178, 119)',
+                    'rgb(150, 222, 149)',
+                    'rgb(42, 101, 42)',
+                    'rgb(214, 237, 212)',
+                    'rgb(40, 45, 41)',
+                ],
+                borderWidth: 1,
+                cutout: '80%'
+            },
+        ],
+    };
+
+    useEffect(() => {
+        fetchData();
+    },[props.refresh]);
+
+    useEffect(() => {
+        if (alarmData){
+            updateChart(alarmData);
+        }
+    },[alarmData]);
+
+    return (<>
+        {loading &&
+            <SkeletonTheme baseColor='#C5EAC5' highlightColor='#D6EDD4'>
+                <Skeleton className={classes.card} containerClassName={classes.cardSkeletonContainer}/>
+            </SkeletonTheme>
+        }
+        { error &&
+            <div className={classes.errorCard}><BiError></BiError>Error loading data!</div>
+        }
+        {alarmData && 
+            <div className={`${classes.card} ${additionalStyles}`}>
             <div className={classes.headingContainer}>
                 <div className={classes.titleContainer}>
                     <h1>Active Alarms</h1>
@@ -93,7 +164,7 @@ function AlarmChart(props){
             </div>
             <div className={classes.contentContainer}>
                 <div className={classes.piechart}>
-                    <Doughnut data={data} plugins={plugins} options={options}/>
+                    {alarmData && <Doughnut data={data} plugins={plugins} options={options} ref={chartReference}/>}
                 </div>
                 <div className={classes.dataContainer}>
                     <div className={classes.data}>
@@ -101,7 +172,7 @@ function AlarmChart(props){
                             Back Flow
                         </div>
                         <div className={classes.dataNumber}>
-                            14%
+                            {alarmData && isNaN((alarmData[1]/alarmData.reduce((acc,el) => acc + el,0)).toFixed(3)*100) ? 0 : (alarmData[1]/alarmData.reduce((acc,el) => acc + el,0)).toFixed(3)*100}%
                         </div>
                     </div>
                     <div className={classes.data}>
@@ -109,7 +180,7 @@ function AlarmChart(props){
                             High Flow
                         </div>
                         <div className={classes.dataNumber}>
-                            39%
+                            {alarmData && isNaN((alarmData[0]/alarmData.reduce((acc,el) => acc + el,0)).toFixed(3)*100) ? 0 : (alarmData[0]/alarmData.reduce((acc,el) => acc + el,0)).toFixed(3)*100}%
                         </div>
                     </div>
                     <div className={classes.data}>
@@ -117,7 +188,7 @@ function AlarmChart(props){
                             Failed Read
                         </div>
                         <div className={classes.dataNumber}>
-                            47%
+                            {alarmData && isNaN((alarmData[2]/alarmData.reduce((acc,el) => acc + el,0)).toFixed(3)*100) ? 0 : (alarmData[2]/alarmData.reduce((acc,el) => acc + el,0)).toFixed(3)*100}%
                         </div>
                     </div>
                     <div className={classes.data}>
@@ -125,7 +196,7 @@ function AlarmChart(props){
                             Battery
                         </div>
                         <div className={classes.dataNumber}>
-                            16%
+                            {alarmData && isNaN((alarmData[3]/alarmData.reduce((acc,el) => acc + el,0)).toFixed(3)*100) ? 0 : (alarmData[3]/alarmData.reduce((acc,el) => acc + el,0)).toFixed(3)*100}%
                         </div>
                     </div>
                     <div className={classes.data}>
@@ -133,13 +204,13 @@ function AlarmChart(props){
                             Temperature
                         </div>
                         <div className={classes.dataNumber}>
-                            14%
+                            {alarmData && isNaN((alarmData[4]/alarmData.reduce((acc,el) => acc + el,0)).toFixed(3)*100) ? 0 : (alarmData[4]/alarmData.reduce((acc,el) => acc + el,0)).toFixed(3)*100}%
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        </div>}
+        </>);
 }
 
 export default AlarmChart;
